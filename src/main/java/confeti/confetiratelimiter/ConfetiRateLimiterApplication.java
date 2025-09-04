3d0f2783a57ff93b3bf1872af5547b27c5f75d95
chyun <och2510@gmail.com>
2025-09-05 00:21:19 +0900

    SERVER-13 init: 프로젝트 초기 생성

---
+++ src/main/java/confeti/confetiratelimiter/ConfetiRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package confeti.confetiratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfetiRateLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfetiRateLimiterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/confeti/confetiratelimiter/ConfetiRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package confeti.confetiratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfetiRateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
