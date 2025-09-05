82cf4c31a2462170bcfbcaa5ea4935a9c928b83a
chyun <och2510@gmail.com>
2025-09-06 01:03:17 +0900

    SERVER-14 feat: OpenFeign 구성 정보 추가

---
+++ src/main/java/confeti/confetiratelimiter/global/config/OpenFeignConfig.java
@@ -0,0 +1,52 @@
+package confeti.confetiratelimiter.global.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import confeti.confetiratelimiter.global.exception.CustomErrorDecode;
+import feign.Logger;
+import feign.Logger.Level;
+import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
+import feign.jackson.JacksonDecoder;
+import feign.optionals.OptionalDecoder;
+import lombok.RequiredArgsConstructor;
+import feign.okhttp.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class OpenFeignConfig {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CustomErrorDecode();
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        ObjectMapper objectMapper =
+                new ObjectMapper()
+                        // 기본 타입에 null이 할당될 경우 에러 무시
+                        .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+                        // enum 값이 없는 경우 null 처리
+                        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+                        // property가 없을 때 에러 발생 무시
+                        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return new OptionalDecoder(new JacksonDecoder(objectMapper));
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Level.BASIC;
+    }
+
+    @Bean
+    public OkHttpClient client() {
+        return new OkHttpClient(
+                new okhttp3.OkHttpClient.Builder()
+                        .build()
+        );
+    }
+}
