e9a4c60076b0ad8560f2d8ea8024be670988a41f
chyun <och2510@gmail.com>
2025-09-18 21:52:19 +0900

    SERVER-14 docs: 로깅 추가

--- src/main/java/confeti/confetiratelimiter/domain/applemusic/application/AppleMusicService.java
+++ src/main/java/confeti/confetiratelimiter/domain/applemusic/application/AppleMusicService.java
@@ -12,8 +12,10 @@ import confeti.confetiratelimiter.global.common.response.ErrorCode;
 import confeti.confetiratelimiter.global.exception.ConfetiException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @CircuitBreaker(name = "appleMusicService", fallbackMethod = "circuitBreakerFallback")
@@ -22,6 +24,7 @@ public class AppleMusicService {
     private final AppleMusicFeignClient client;
 
     public AppleMusicArtistResponse getArtistById(String id) {
+        log.info("AppleMusicService.getArtistById Single artist lookup started. Target artist ID : {}", id);
         AppleMusicArtistsResponse appleMusicArtistsResponse = client.getArtistById(id);
 
         return appleMusicArtistsResponse.data().stream().findFirst()
@@ -31,30 +34,37 @@ public class AppleMusicService {
     }
 
     public AppleMusicArtistsResponse getArtistsByIds(String ids) {
+        log.info("AppleMusicService.getArtistsByIds Multiple artists lookup started. Target artist IDs : {}", ids);
         return client.getArtistsByIds(ids);
     }
 
     public AppleMusicArtistsResponse getRelatedArtistsById(String id, String view, String limit) {
+        log.info("AppleMusicService.getRelatedArtistsById Related artists lookup started. Target artist IDs : {}, View : {}, Limit : {}", id, view, limit);
         return client.getRelatedArtistsById(id, view, limit);
     }
 
     public AppleMusicArtistMusicsResponse getArtistMusicsById(String id, String limit, String offset) {
+        log.info("AppleMusicService.getArtistMusicsById Artist musics lookup started. Target artist ID : {}, Limit : {}, Offset : {}", id, limit, offset);
         return client.getArtistMusicsById(id, limit, offset);
     }
 
     public AppleMusicAlbumsResponse getAlbumsByIds(String ids) {
+        log.info("AppleMusicService.getAlbumsByIds Albums lookup started. Target album IDs : {}", ids);
         return client.getAlbumsByIds(ids);
     }
 
     public AppleMusicMusicsResponse getSongsByIds(String ids) {
+        log.info("AppleMusicService.getSongsByIds Songs lookup started. Target song IDs : {}", ids);
         return client.getSongsByIds(ids);
     }
 
     public AppleMusicSearchResponse searchByKeyword(String term, String types, String limit, String offset, String with) {
+        log.info("AppleMusicService.searchByKeyword Search started. Term : {}, Types : {}, Limit : {}, Offset : {}, Width : {}", term, types, limit, offset, with);
         return client.searchByKeyword(term, types, limit, offset, with);
     }
 
     public AppleMusicChartsResponse getCharts(String types, String limit) {
+        log.info("AppleMusicService.getCharts Charts lookup started. Types : {}, Limit : {}", types, limit);
         return client.getCharts(types, limit);
     }
 
