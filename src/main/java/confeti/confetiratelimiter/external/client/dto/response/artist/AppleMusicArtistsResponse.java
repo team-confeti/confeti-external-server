4876c910e8767e9155bd4ff80cdc3a573e063d81
jher235 <tim668666@gmail.com>
2026-01-02 03:37:15 +0900

    CONFETI-76 feat: next 필드 추가 - similar artist 같은 next 가 필요한 데이터의 경우가 존재하여 추가함

--- src/main/java/confeti/confetiratelimiter/external/client/dto/response/artist/AppleMusicArtistsResponse.java
+++ src/main/java/confeti/confetiratelimiter/external/client/dto/response/artist/AppleMusicArtistsResponse.java
@@ -3,6 +3,10 @@ package confeti.confetiratelimiter.external.client.dto.response.artist;
 import java.util.List;
 
 public record AppleMusicArtistsResponse(
+        String next,
         List<AppleMusicArtistResponse> data
 ) {
+    public AppleMusicArtistsResponse(List<AppleMusicArtistResponse> data){
+        this(null, data);
+    }
 }
